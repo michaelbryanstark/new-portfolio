fd008a54216477dfc249cdc66a4791c8245423d6
Michael Stark <MStark@phillips.com>
2023-03-11 12:53:43 -0500

    added projects to project component, added data to data.js

--- src/App.js
+++ src/App.js
@@ -2,20 +2,17 @@ import React from 'react';
 import About from "./components/About";
 // import Contact from "./components/Contact";
 // import Navbar from "./components/Navbar";
-// import Projects from "./components/Projects";
+import Projects from "./components/Projects";
 // import Skills from "./components/Skills";
 // import Testimonials from "./components/Testimonials";
 
 export default function App() {
   return (
     <main className="text-gray-600 bg-blue-100 body-font">
-      {<About />
-      /* <Navbar />
-      
+      <About />
       <Projects />
-      <Skills />
-      <Testimonials />
-      <Contact /> */}
+      
+      
       
     </main>
   );
---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const projects = [
+    {
+      title: "BDS Quality Services",
+      subtitle: "React",
+      description:
+        "Built using the React.js, JavaScript, and HTML/CSS, BDS Quality Services is a company that provides quality assurance training and compliance.",
+      image: "./bdsqs.jpg",
+      link: "https://bdsqs.net",
+    },
+    {
+      title: "CodeShare",
+      subtitle: "React",
+      description:
+        "Full stack React app built for developers to share code. Features full CRUD built using JavaScript, Node.js, CSS, MongoDB/Mongoose, JWT-based Authentication.",
+      image: "./codeshare.jpg",
+      link: "https://codeshare-app3.herokuapp.com/login",
+    },
+    {
+      title: "The Penalty Box",
+      subtitle: "Python and Django",
+      description:
+        "Personal Blog with full CRUD built using the Django Framework and Python. Users can sign up leave comments on blog posts.",
+      image: "./pirate.png",
+      link: "https://penalty-box.herokuapp.com/",
+    },
+    {
+    //   title: "Epic Todo App",
+    //   subtitle: "React Hooks",
+    //   description:
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    //   image: "./project-4.gif",
+    //   link: "https://pythonbootcamp.com",
+    },
+  ];
