2175f3408605e7961f2c2fb246bc1452fcb5c138
michaelbryanstark <84143645+michaelbryanstark@users.noreply.github.com>
2023-03-12 00:29:58 -0500

    changed a to div line 8 navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,11 @@ export default function Navbar() {
   return (
     <header className="bg-[#061624] opacity-90 sticky top-0 z-50">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="title-font font-medium text-[#DEB992] mb-4 md:mb-0">
+        <div className="title-font font-medium text-[#DEB992] mb-4 md:mb-0">
           <a href="#about" className="ml-3 text-xl">
             Michael Stark
           </a>
-        </a>
+        </div>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-[#1BA098] justify-center">
           <a href="#projects" className="mr-5 hover:text-white">
             Past Work
