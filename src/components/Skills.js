8466105f0a770b537d36aec0d83bc6a6074c9559
michaelbryanstark <84143645+michaelbryanstark@users.noreply.github.com>
2023-03-12 00:53:55 -0500

    skills working, css changes for uniformity

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,14 +5,14 @@ import { skills } from "../data";
 
 export default function Skills() {
   return (
-    <section id="skills pt-10">
+    <section id="skills" className="bg-[#061624] body-font pt-10">
       <div className="container px-5 py-10 mx-auto">
         <div className="text-center mb-20">
 
-          <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
+          <h1 className="sm:text-4xl text-3xl font-medium title-font text-[#DEB992] mb-4">
             Skills &amp; Technologies
           </h1>
-          <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+          <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-[#DEB992]">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi sit
             ipsa delectus eum quo voluptas aspernatur accusantium distinctio
             possimus est.
@@ -23,7 +23,7 @@ export default function Skills() {
             <div key={skills.skill} className="p-2 sm:w-1/2 w-full">
               <div className="bg-gray-800 rounded flex p-4 h-full items-center">
 
-                <span className="title-font font-medium text-white">
+                <span className="title-font font-medium text-[#1BA098]">
                   {skills.skill}
                 </span>
               </div>
