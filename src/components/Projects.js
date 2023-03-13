65a6827a9466e3cbceb36d2dfc50388b71c22218
Michael Stark <MStark@phillips.com>
2023-03-13 17:56:22 -0400

    removed irrelevant pics, resized relevantones, used flexboxs to center items and css to keep boxes same size

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,7 +6,7 @@ export default function Projects() {
   return (
     <section id="projects" className="bg-[#061624] body-font pt-20">
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
-        <div className="flex flex-col w-full mb-10">
+        <div className="flex flex-col w-full mb-20">
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-[#DEB992]">
             Apps I've Built
           </h1>
@@ -15,19 +15,19 @@ export default function Projects() {
             and am always looking for others to dive in to!
           </p>
         </div>
-        <div className="flex flex-wrap -m-4">
+        <div className="flex flex-wrap justify-center -m-4">
           {projects.map((project) => (
             <a
               href={project.link} target="_blank" rel="noreferrer"
               key={project.image}
-              className="sm:w-1/2 w-100 p-6">
-              <div className="flex relative">
+              className="p-6">
+              <div className="flex relative max-w-md">
                 <img
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-fill rounded-lg object-center"
+                  className="absolute rounded-lg inset-0 w-full h-full object-fill object-center"
                   src={project.image}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                <div className="max-w-md px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 rounded-lg opacity-0 hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-[#DEB992] mb-1">
                     {project.subtitle}
                   </h2>
