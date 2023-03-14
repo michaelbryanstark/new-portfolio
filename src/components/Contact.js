1e819b0aa6d1db2fe78d97667ffd122ac6551bf4
Michael Stark <MStark@phillips.com>
2023-03-14 15:45:57 -0400

    connected emailjs to contact form and tested to make sure data was being forwarded to gmail

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,30 +1,37 @@
 
-import React from "react";
+import React, { useState } from "react";
 import { SocialIcon } from 'react-social-icons';
+import emailjs from 'emailjs-com'
 
 export default function Contact() {
   const [name, setName] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [message, setMessage] = React.useState("");
+  const [emailSent, setEmailSent] = useState(false);
+  const submit = () => {
+    if (name && email && message) {
+      const serviceId = 'service_2rh69zk';
+      const templateId = 'template_ighzcwc';
+      const userId = 'GWelQe7mDCutjawxE';
+      const templateParams = {
+          name,
+          email,
+          message
+      };
 
-  function encode(data) {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&");
-  }
+      emailjs.send(serviceId, templateId, templateParams, userId)
+          .then(response => console.log(response))
+          .then(error => console.log(error))
+          alert("Message Sent");
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", name, email, message }),
-    })
-      .then(() => alert("Message sent!"))
-      .catch((error) => alert(error));
-  }
+        setName('');
+        setEmail('');
+        setMessage('');
+        setEmailSent(false);
+    } else {
+        alert('Please fill in all fields.');
+    }
+}
 
   return (
     <>
@@ -55,10 +62,8 @@ export default function Contact() {
             </div>
           </div>
         </div>
-        <form
-          netlify
+        <div
           name="contact"
-          onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-2 mt-8 md:mt-0">
           <h2 className="text-[#DEB992] sm:text-4xl text-3xl mb-1 font-medium title-font">
             Get In Touch
@@ -76,8 +81,8 @@ export default function Contact() {
               type="text"
               id="name"
               name="name"
-              className="w-full bg-blue-100 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setName(e.target.value)}
+              className="w-full bg-blue-100 rounded border border-gray-700 focus:border-[#1BA098] focus:ring-2 focus:ring-[#1BA098] text-[#061624] outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              value={name} onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="relative mb-4">
@@ -88,8 +93,8 @@ export default function Contact() {
               type="email"
               id="email"
               name="email"
-              className="w-full bg-blue-100 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setEmail(e.target.value)}
+              className="w-full bg-blue-100 rounded border border-gray-700 focus:border-[#1BA098] focus:ring-2 focus:ring-[#1BA098] text-[#061624] outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              value={email} onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="relative mb-4">
@@ -101,16 +106,17 @@ export default function Contact() {
             <textarea
               id="message"
               name="message"
-              className="w-full bg-blue-100 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setMessage(e.target.value)}
+              className="w-full bg-blue-100 rounded border border-gray-700 focus:border-[#1BA098] focus:ring-2 focus:ring-[#1BA098] h-32 text-[#061624] outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              value={message} onChange={(e) => setMessage(e.target.value)}
             />
           </div>
           <button
             type="submit"
-            className="text-white bg-[#1BA098] border-0 py-2 px-6 focus:outline-none hover:bg-[#DEB992] rounded text-lg">
+            className="text-white bg-[#1BA098] border-0 py-2 px-6 focus:outline-none hover:bg-[#DEB992] rounded text-lg" onClick={submit}>
             Submit
           </button>
-        </form>
+          <span className={emailSent ? 'visible' : null}></span>
+        </div>
       </div>
     </section>
     <div className="flex flex-wrap justify-center">
