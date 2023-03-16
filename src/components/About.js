d085ee369c507f52feca6ce20e0d10a8b067a17c
michaelbryanstark <84143645+michaelbryanstark@users.noreply.github.com>
2023-03-15 21:47:09 -0400

    added heroicons to contact, project, skills, added github link on headshot

--- src/components/About.js
+++ src/components/About.js
@@ -16,8 +16,8 @@ export default function About() {
     }, []);
   return (
 
-    <section id="about">
-      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+    <section id="about" className="pt-20">
+      <div className="container mx-auto flex px-10 py-5 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">   
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-[#DEB992]">
             Hi, I'm Michael.
@@ -45,11 +45,13 @@ export default function About() {
           </div>
         </div>
         <div className="lg:max-w-lg bg-[#061624] lg:w-full md:w-1/2 w-5/6 py-20">
-          <img
-            className="object-cover rounded-lg object-center "
-            alt="hero"
-            src="./84143645.jpg"
-          />
+            <a href="https://github.com/michaelbryanstark" target="_blank" rel="noreferrer">
+                <img
+                    className="object-cover rounded-lg object-center "
+                    alt="hero"
+                    src="./84143645.jpg"
+                />
+            </a>
         </div>
       </div>
       
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -35,13 +35,16 @@ export default function Contact() {
 
   return (
     <>
-    <section id="contact" className="relative">
-      <div className="container px-10 py-24 pt-24 mx-auto flex sm:flex-nowrap flex-wrap">
-        <div className="lg:w-2/3 md:w-1/2 overflow-hidden sm:mr-10 p-10  flex items-end justify-start relative">
+    <section id="contact" className="relative pt-20">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="flex flex-wrap justify-center text-[#DEB992] mx-auto inline-block w-10 mb-4">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M9 3.75H6.912a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859M12 3v8.25m0 0l-3-3m3 3l3-3" />
+    </svg>
+    <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+        <div className="lg:w-2/3 md:w-1/2 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
           <img
             width="100%"
             height="100%"
-            className="absolute inset-0 rounded"
+            className="absolute inset-0 rounded pt-5"
             src="./brooklyn.jpg"
             alt="brooklyn"
           />
