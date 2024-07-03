f1ac36f9e71d61808354c21f942973f52d073c89
michaelbryanstark <84143645+michaelbryanstark@users.noreply.github.com>
2024-07-03 12:21:36 -0400

    changed text color of skills title

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,11 +7,11 @@ export default function Skills() {
     <section id="skills" className="bg-[#061624] body-font pt-20">
       <div className="container px-5 py-5 mx-auto">
         <div className="text-center mb-20">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="flex flex-wrap justify-center text-[#061623] mx-auto inline-block w-10 mb-4">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="flex flex-wrap justify-center text-[#DEB992] mx-auto inline-block w-10 mb-4">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3" />
           </svg>
-          <h1 className="sm:text-4xl text-3xl font-medium title-font text-[#061623] mb-4">
-            Skills &amp; Technologies
+          <h1 className="sm:text-4xl text-3xl font-medium title-font text-[#DEB992] mb-4">
+            Skills & Technologies
           </h1>
           <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-[#061623]">
        
